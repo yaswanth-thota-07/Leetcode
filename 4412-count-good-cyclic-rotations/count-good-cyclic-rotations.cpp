82eab6f6eee6e29bece3aba14b7979aac45a1016
Yashwanth Thota <thota5366@gmail.com>
2026-09-06 11:11:05 +0530

    Time: 2 ms (70.98%) | Memory: 103.2 MB (66.49%) - LeetSync

---
+++ 4412-count-good-cyclic-rotations/count-good-cyclic-rotations.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int countGoodRotations(vector<int>& nums) {
+        int n = nums.size();
+        long long sum1 = 0;
+        long long sum2 = 0;
+        for(int i = 0;i<n/2;i++){
+            sum1 = sum1+ nums[i];
+        }
+         for(int i = n/2;i<n;i++){
+            sum2 = sum2+ nums[i];
+        }
+        int lp = 0;
+        int hp = n/2;
+        int count1 = 0;
+        int count2 = 0;
+        if(sum1>sum2){count1++;}
+        for(int i = 0;i<n-1;i++){
+           sum1 =  sum1-nums[lp]+nums[hp];
+           sum2 =  sum2-nums[hp]+nums[lp]; 
+            lp = (lp + 1) % n;
+            hp = (hp + 1) % n;
+           if(sum1>sum2){
+            count1++;
+           }     
+        }
+        return count1;
+    }
+};
